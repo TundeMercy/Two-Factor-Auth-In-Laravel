14996f680579af2fb58fb2a0763c1d340bbe8539
Tunde Ajagbe <tundeajagbea@gmail.com>
2021-06-30 02:57:43 +0100

    Retactoring: Enables forms error reporting

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -2,12 +2,18 @@
 
 @section('content')
     <h2>Confirm Your Password</h2>
-    <form method="POST" action="{{ url('user/confirm-password') }}">
-        @csrf
-        <div class="mb-3">
-            <label for="password" class="form-label">Password</label>
-            <input type="password" name="password" class="form-control" id="password">
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    <div class="centered-container">
+        <form method="POST" action="{{ url('user/confirm-password') }}">
+            @csrf
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" name="password" class="form-control @error('password') is-invalid @enderror"
+                       id="password">
+                @error('password')
+                {{ $message }}
+                @enderror
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
 @endsection
